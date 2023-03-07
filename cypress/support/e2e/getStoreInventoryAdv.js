75fef699b0e91c5ae7c7d66a87ecfd6e75feec49
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-03-07 12:10:53 +0700

    API Testing GET - Add more test case on Get Store Inventory advanced testing

--- cypress/support/e2e/getStoreInventoryAdv.js
+++ cypress/support/e2e/getStoreInventoryAdv.js
@@ -11,4 +11,28 @@ describe('PetStore API Testing - Store Inventory - GET', () => {
             expect(inventory.status).to.equal(200);
         })
     })
+
+    it('Validate Response Header', () => {
+        // assertion
+        cy.get('@getInventory').then(inventory => {
+            expect(inventory).to.have.property('headers');
+            expect(inventory.headers['content-type']).to.equal('application/json');
+        })
+    })
+
+    it('Validate Response Body', () => {
+        // assertion
+        cy.get('@getInventory').then(inventory => {
+            expect(inventory).to.be.an('object');
+        })
+    })
+
+    it('Validate Response Body Object should have correct Property & Value', () => {
+        // assertion
+        cy.get('@getInventory').then(inventory => {
+            expect(inventory.body).to.haveOwnProperty('available');
+            expect(inventory.body).to.haveOwnProperty('pending');
+            expect(inventory.body.pending).to.be.a('number');
+        })
+    })
 })
